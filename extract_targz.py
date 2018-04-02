f0222e2d06d8853fb172d8e384aea334b3062b87
Luis Puentes <luispuentesvega@gmail.com>
2018-04-02 15:31:34 +0700

    New Functions

--- extract_targz.py
+++ extract_targz.py
@@ -1,4 +1,14 @@
+import os
 import tarfile
-tar = tarfile.open("samples.tar.gz")
-tar.extractall('D://Luis/Programming/Python/util-scripts-py/20180329')
+import stat
+file = 'example.tar.gz'
+st = os.stat(file)
+oct_perm = oct(st.st_mode)
+print(oct_perm)
+print(file)
+"""
+tar = tarfile.open(file)
+rta = tar.extractall('D://Luis/Programming/Python/util-scripts-py/20180329')
+print(rta)
 tar.close()
+"""
---
+++ read_csv_file_from_second_row.py
@@ -0,0 +1,17 @@
+import os
+import csv
+import matplotlib.pyplot as plt
+
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+
+labels = []
+sizes = []
+with open(os.getcwd()+'/files/people.csv') as csvfile:
+    next(csvfile)#Next Line
+    row_csv = csv.reader(csvfile, delimiter=',')
+    for row in row_csv:
+        labels.append(row[0])
+        sizes.append(float(row[1]))
+
+print(labels)
+#['Luis', 'Maria', 'Camila', 'Peter']
