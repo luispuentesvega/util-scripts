b297589516dc983f83c0bae2fd0abd916a90366c
Luis Puentes <luispuentesvega@gmail.com>
2018-10-12 20:53:15 +0700

    New Functions

--- clean_specific_directory.py
+++ clean_specific_directory.py
@@ -1,10 +1,9 @@
 import os, re, os.path
 
 def main():
-	mypath = "C://Users/ruok/Downloads/"
+	mypath = "C://Users/folder/"
 	for root, dirs, files in os.walk(mypath):
 		for file in files:
 			os.remove(os.path.join(root, file))
 			
 main()
-#Response : File has been found here  C://Users/xxx/folder\
--- find_specific_folder.py
+++ find_specific_folder.py
@@ -3,7 +3,7 @@ import ntpath
 
 def main():
 	folderName = "node_modules"
-	for root,dirs,files in os.walk("C://Users/ruok/Projects/"):
+	for root,dirs,files in os.walk("C://folder/"):
 		for folder in dirs:
 			if folderName in folder:
 				print(counter,".The Folder has been found here ", r)
